231d8775c859a22a8e5b2da4dc70e0a7725f6eea
Pranit Patil <pranitpatil.jobs@gmail.com>
2021-09-27 13:37:45 +0200

    Adding version for optimistic locking

--- src/main/java/com/pranitpatil/entity/AuditableEntity.java
+++ src/main/java/com/pranitpatil/entity/AuditableEntity.java
@@ -3,10 +3,7 @@ package com.pranitpatil.entity;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -14,15 +11,16 @@ import java.util.Date;
 @MappedSuperclass
 public class AuditableEntity {
 
-    //@Temporal(value = TemporalType.TIMESTAMP)
     @Column(updatable = false, name = "CREATED_AT")
     @CreationTimestamp
     private LocalDateTime createdAt;
 
-    //@Temporal(value = TemporalType.TIMESTAMP)
     @Column(updatable = true, name = "LAST_MODIFIED_AT")
     @UpdateTimestamp
     private LocalDateTime lastModifiedAt;
+
+    @Version
+    private int version;
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
